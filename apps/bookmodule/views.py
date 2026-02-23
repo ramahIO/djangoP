2d85b3a8b32d764449937267e0d42f0d8f87d2b7
ramahIO <amramah9@gmail.com>
2026-02-23 16:56:07 +0300

    lab3 task 5

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,10 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html")
-
-
-def index2(request, val1 = 0): #add the view function (index2)
- return HttpResponse("value1 = "+str(val1))
+    context = {"name": name}
+    return render(request, "bookmodule/index.html", context)
