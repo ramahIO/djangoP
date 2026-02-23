291ee724cd6b9fcdab80bd4ffe3f329dee8c6961
ramahIO <amramah9@gmail.com>
2026-02-23 16:23:57 +0300

    lab3 task 3

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
 def index(request):
- name = request.GET.get("name") or "Ramah!" 
- return HttpResponse("Hello, "+ name) 
+ name = request.GET.get("name") or "Ramah!"
+ return HttpResponse("Hello, "+name)
+def index2(request, val1 = 0): #add the view function (index2)
+ return HttpResponse("value1 = "+str(val1))
--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -23,4 +23,5 @@ import apps.bookmodule.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', apps.bookmodule.views.index),
+    path('index2/<int:val1>/', apps.bookmodule.views.index2) 
 ]
