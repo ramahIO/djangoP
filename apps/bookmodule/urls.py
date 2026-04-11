d603e0b66fbc671c15045a1f8f5da2cf7315c1b6
ramahIO <amramah9@gmail.com>
2026-04-12 02:46:04 +0300

    lab7

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -2,10 +2,19 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
  path('', views.index, name= "books.index"),
  path('list_books/', views.list_books, name= "books.list_books"),
  path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
  path('aboutus/', views.aboutus, name="books.aboutus"),
-]
+ path('html5/links/', views.links, name='links'),
+path("html5/text/formatting/", views.formatting, name="books.formatting"),
+path("html5/listing/", views.listing, name="books.listing"),
+path("html5/tables/",views.tables, name= "book.tables"),
+path('search', views.searchBooks, name='book-search'),
+path('insert/', views.insertBooks, name='book-insert'),
+path('simple/query', views.simple_query, name='simple-query'),
+path('complex/query', views.complex_query, name='complex-query'),
 
+]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse   # لو لسه تحتاجين index2، وإلا تقدرين تحذفينه
+from .models import Book
 
 # لو لسه تبين تحتفظين بـ index2 من لاب 3:
 def index2(request, val1=0):
@@ -20,3 +21,62 @@ def viewbook(request, bookId):
 def aboutus(request):
     return render(request, 'bookmodule/aboutus.html')
 
+
+def links(request):
+    return render(request, 'bookmodule/links.html')
+
+def formatting(request):
+    return render(request, 'bookmodule/formatting.html')
+
+def listing(request):
+    return render(request, "bookmodule/listing.html")
+
+def tables(request):
+    return render(request, "bookmodule/tables.html")
+
+def searchBooks(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+
+        books = __getBooksList()
+        newBooks = []
+
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower(): contained = True
+            if not contained and isAuthor and string in item['author'].lower(): contained = True
+
+            if contained: newBooks.append(item)
+
+        return render(request, 'bookmodule/bookList.html', {'books': newBooks})
+
+    return render(request, 'bookmodule/search.html')
+
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765, 'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
+
+
+def insertBooks(request):
+    Book.objects.create(title='Continuous Delivery', author='J.Humble and D. Farley', price=120.00, edition=3)
+    Book.objects.create(title='Reversing: Secrets of Reverse Engineer', author='E. Eilam', price=97.00, edition=2)
+    Book.objects.create(title='The Hundred-Page Machine Learning Book', author='Andriy Burkov', price=100.00, edition=4)
+    return render(request, 'bookmodule/bookList.html', {'books': Book.objects.all()})
+
+def simple_query(request):
+    mybooks = Book.objects.filter(title__icontains='the')
+    return render(request, 'bookmodule/bookList.html', {'books': mybooks})
+
+def complex_query(request):
+    mybooks = Book.objects.filter(author__isnull=False)\
+                          .filter(title__icontains='the')\
+                          .filter(edition__gte=2)\
+                          .exclude(price__lte=50)[:10]
+    if len(mybooks) >= 1:
+        return render(request, 'bookmodule/bookList.html', {'books': mybooks})
+    else:
+        return render(request, 'bookmodule/index.html')
